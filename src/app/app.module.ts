8f59b64a84167c0043652ce15b987eac1bbe17f4
DanielCaz <cazarez.jorge.prog2019@gmail.com>
2022-04-06 13:00:50 -0500

    Manipulación HTML

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ContadorModule } from './contador/contador.module';
 import { CalculadoraComponent } from './contador/calculadora/calculadora.component';
 import { PokemonModule } from './pokemon/pokemon.module';
 import { BootstrapComponent } from './bootstrap/bootstrap.component';
+import { HtmlHandlerComponent } from './html-handler/html-handler.component';
 
 @NgModule({
-  declarations: [AppComponent, CalculadoraComponent, BootstrapComponent],
-  imports: [BrowserModule, ContadorModule, PokemonModule],
+  declarations: [
+    AppComponent,
+    CalculadoraComponent,
+    BootstrapComponent,
+    HtmlHandlerComponent,
+  ],
+  imports: [BrowserModule, ContadorModule, PokemonModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ src/app/html-handler/html-handler.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-html-handler',
+  templateUrl: './html-handler.component.html',
+  styleUrls: ['./html-handler.component.css'],
+})
+export class HtmlHandlerComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  nombre: string = '';
+  apellido: string = '';
+  edad: number = 0;
+
+  guardar(event: KeyboardEvent, nombre: string) {
+    if (event.key === 'Enter') {
+      this.nombre = nombre;
+    }
+  }
+
+  almacenar() {
+    console.log('Hola');
+  }
+}
